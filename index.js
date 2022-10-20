d61188c9de65f49be5f006d38924f813cb44be97
rossivk <rosica.kirkova@gmail.com>
2022-10-21 00:17:27 +0100

    many

--- index.js
+++ index.js
@@ -1,53 +1,78 @@
 
+window.addEventListener("load", myFunction);
+window.addEventListener("resize", myFunction);
+
+function myFunction() {
+  const backgroundMenu = document.getElementsByClassName("nav-container");
+  let tl1 = new TimelineLite({ paused: true, reversed: true });
+
+  if (window.innerWidth <= 800) {
+    document.body.style.backgroundColor = "pink";
+    tl1.from(backgroundMenu, 0.5, {
+      opacity: 0, x: 750
+    });
+
+    // Hamburger timeline
+
+    const upper = document.getElementsByClassName("upper");
+    const middle = document.getElementsByClassName("middle");
+    const middle1 = document.getElementsByClassName("middle1");
+    const lower = document.getElementsByClassName("lower");
+    let tl2 = new TimelineLite({ paused: true, reversed: true });
+
+    tl2
+      .to(
+        upper,
+        0.5,
+        {
+          attr: { d: "M11,2 L2,11" },
+          x: 1,
+          stroke: `#191919`,
+          strokeWidth: 2,
+          ease: Power2.easeInOut
+        },
+        "start"
+      )
+
+      .to(middle, 0.5, { autoAlpha: 0, stroke: `#191919` }, "start")
+      .to(middle1, 0.5, { autoAlpha: 0, stroke: `#CB5302` }, "start")
+      .to(
+        lower,
+        0.5,
+        {
+          attr: { d: "M2,2 L11,11" },
+          x: 1,
+          stroke: `#CB5302`,
+          strokeWidth: 2,
+          ease: Power2.easeInOut
+        },
+        "start"
+      );
+
+    // attach the action to the hamburger icon
+
+
+    document
+      .querySelector(".hamburger-icon")
+      .addEventListener("click", function () {
+        tl2.reversed() ? tl2.play() && tl1.play() : tl2.reverse() && tl1.reverse();
+      });
+  }
+  else {
+    tl1.from(backgroundMenu, 0.5, {
+      opacity: 1, x: 0
+    });
+  }
+}
+
 // Nav timeline
-const backgroundMenu = document.getElementsByClassName("nav-container");
-let tl1 = new TimelineLite({ paused: true, reversed: true });
-tl1.from(backgroundMenu, 0.5, { opacity: 1, x: 0 });
 
-// Hamburger timeline
 
-const upper = document.getElementsByClassName("upper");
-const middle = document.getElementsByClassName("middle");
-const middle1 = document.getElementsByClassName("middle1");
-const lower = document.getElementsByClassName("lower");
-let tl2 = new TimelineLite({ paused: true, reversed: true });
-
-tl2
-  .to(
-    upper,
-    0.5,
-    {
-      attr: { d: "M11,2 L2,11" },
-      x: 1,
-      stroke: `#191919`,
-      strokeWidth: 2,
-      ease: Power2.easeInOut
-    },
-    "start"
-  )
-
-  .to(middle, 0.5, { autoAlpha: 0, stroke: `#191919` }, "start")
-  .to(middle1, 0.5, { autoAlpha: 0, stroke: `#CB5302` }, "start")
-  .to(
-    lower,
-    0.5,
-    {
-      attr: { d: "M2,2 L11,11" },
-      x: 1,
-      stroke: `#CB5302`,
-      strokeWidth: 2,
-      ease: Power2.easeInOut
-    },
-    "start"
-  );
-
-// attach the action to the hamburger icon
-
-document
-  .querySelector(".hamburger-icon")
-  .addEventListener("click", function () {
-    tl2.reversed() ? tl2.play() && tl1.reverse() : tl2.reverse() && tl1.play();
-  });
+// document
+// .querySelector(".hamburger-icon")
+// .addEventListener("click", function () {
+//   tl2.reversed() ? tl2.play() && tl1.play() : tl2.reverse() && tl1.reverse();
+// });
 
 
 
